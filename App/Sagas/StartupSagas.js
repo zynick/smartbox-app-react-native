acb04c01564c07331cb6abbdbe18b3f73ecf489f
Zi-Yuan Chan <ziyuan@zynick.com>
2017-03-26 07:17:24 +0800

    remove fullbutton, rounded buttons, and Lib/ directory

--- App/Sagas/StartupSagas.js
+++ App/Sagas/StartupSagas.js
@@ -1,7 +1,7 @@
 import { put, select } from 'redux-saga/effects'
 import StartupActions from '../Redux/StartupRedux'
 // import TemperatureActions from '../Redux/TemperatureRedux'
-import { is } from 'ramda'
+// import { is } from 'ramda'
 
 // // exported to make available for tests
 // export const selectTemperature = state => state.temperature.temperature
